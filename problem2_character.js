8b8b500ee1589dac98225b88c9cd2c294ab126e4
tertius Jubber <tjubber@gmail.com>
2023-06-13 08:55:56 +0200

    "JSway ch09 problems"

---
+++ problem2_character.js
@@ -0,0 +1,23 @@
+//Character inventot=ry 
+//Improve the example RPG to add character inventory management according to the following rules:
+//A character's inventory contains a number of gold and a number of keys.
+//Each character description must show the inventory state.
+//When a character slays another character , the victim's inventory goes to its vanquisher.
+
+class MyClass {
+  constructor(param1,param2,etc){
+    this.property1 = param1;
+    this.property2 = param2;
+  }
+  method1(){
+
+  }
+  method2(){
+
+  }
+}
+
+const myObject = new MyClass(arg1,arg2,etc);
+myObject.method1()
+
+console.log(this);
